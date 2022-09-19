90386f6b55b224a4a3abe9d423419eb4b1a9f8a6
Sunney1431 <cokersunday36@gmail.com>
2022-09-19 16:51:51 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,17 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: input string.
- * Return: length of a string.
+ * @s: string.
+ * Return: returns length as integer;
  */
-int_strlen(char *s)
+int _strlen(char *s)
 {	
 
 	int len = 0;
 
 	while (*(s + len) != '\0')
 		len++;
+
 	return (len);
 }
 
