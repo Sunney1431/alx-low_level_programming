89fd8cf6e689c4cb0685123014bb71f7f8511172
Sunney1431 <cokersunday36@gmail.com>
2022-09-21 07:11:38 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,9 @@
 
 /**
   *_strcat - concatenates the string pointed to by @src to
-    the end of the string pointed to by @dest
-  *@sre: String to be concatenated upon
+  * the end of the string pointed to by @dest
+  *@dest: String that will be appended
+  *@src: String to be concatenated upon
   *
   * Return: returns poiner to @dest
   */
@@ -13,11 +14,11 @@ char *_strcat (char *dest, char *src)
 
 	int index = 0, dest_len = 0;
 
-	while (dest [index++])
+	while (dest[index++])
         	dest_len++;
 
 	for (index = 0; src[index]; index++)
-		dest [dest_Ten++] = sre[index];
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }	
