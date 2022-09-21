ff3f0af3b9a03eb6aedc2f12a337616836c73f18
Sunney1431 <cokersunday36@gmail.com>
2022-09-21 15:50:46 -0700

    0 task

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,9 +6,6 @@
 	 *int prototypes()
 	 *char prototypes()
 	 */
-	void reverse_array(int *a int n);
-	void print_number(int n);
-	void print_buffer(char *b, int size);
 	int _strcmp(char *s1, char *s2);
 	char *_strcat(char *dest, char *src);
 	char *_strncat(char *dest, char *src, int n);
