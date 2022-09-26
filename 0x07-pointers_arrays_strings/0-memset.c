efeac6661840f273d6ab500d1a8fd299d16d2253
Sunney1431 <cokersunday36@gmail.com>
2022-09-26 14:14:24 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,12 +8,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
-	
-	while (i < n)
+	unsigned int i;
+
+	for(i = 0; n > 0; i++, n--)
 	{
-		*(s + i) = b;
-		i++;
+		s[i] = b;
 	}
+
 	return (s);
 }
